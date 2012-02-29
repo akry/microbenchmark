b9af526993b8f2dc3509f47f9288bac7394e63d0
Akira Yokokawa <akira.yokokawa@gmail.com>
2012-02-29 12:22:51 +0100

    first commit

---
+++ Microbenchmark/src/producerconsumer/Main.java
@@ -0,0 +1,14 @@
+package producerconsumer;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread a = new MyThread();
+		a.start();
+	}
+
+}
