70f116c64e9dceeb318faeed5466daac6e7272f6
Akira Yokokawa <akira.yokokawa@gmail.com>
2012-05-04 11:19:25 +0200

    set time to spin for each thread

--- Microbenchmark/src/busywait/BusyThread.java
+++ Microbenchmark/src/busywait/BusyThread.java
@@ -5,23 +5,28 @@ import java.lang.Thread;
 public class BusyThread extends Thread {
 	
 	final private int id;
-	final private int loop;
+	final private int time;
 	
-	public BusyThread(final int _id, final int _loop) {
-		loop = _loop;
+	public BusyThread(final int _id, final int _time) {
+		time = _time;
 		id = _id;
 	}
 	
 	public void run() {
 		System.out.println("Thread " + id);
+		final long start = System.currentTimeMillis();
 		
-		for(int i=0; i<loop; i++) {
-			for(int j=0; j<loop; j++) {
-				for(int m=0; m<loop; m++) {
-					
-				}
-			}
+		while(System.currentTimeMillis()-start < time*1000) {
+			
 		}
+		
+//		for(int i=0; i<loop; i++) {
+//			for(int j=0; j<loop; j++) {
+//				for(int m=0; m<loop; m++) {
+//
+//				}
+//			}
+//		}
 		
 		System.out.println("Thread " + id + " terminated...");
 	}
--- Microbenchmark/src/busywait/Main.java
+++ Microbenchmark/src/busywait/Main.java
@@ -7,16 +7,15 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(args.length != 2) {
+		if(args.length-1 != Integer.parseInt(args[0])) {
 			System.err.println("wrong argments");
 			System.exit(0);
 		}
 		
 		final int concurrency = Integer.parseInt(args[0]);
-		final int loop = Integer.parseInt(args[1]);
 		
 		for(int i=0; i<concurrency; i++) {
-			new BusyThread(i, loop).start();
+			new BusyThread(i, Integer.parseInt(args[i+1])).start();
 		}
 		
 	}
